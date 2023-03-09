f57bffc588bf3cbf8036fd9197e8c8b3cb253516
Shraddha-Pandey <91560433+Shraddha-Pandey@users.noreply.github.com>
2023-03-09 12:19:00 +0530

    Update nthFibonacciNumber.cpp

--- Dynamic Programming/nthFibonacciNumber.cpp
+++ Dynamic Programming/nthFibonacciNumber.cpp
@@ -1,3 +1,28 @@
+//Bottom up - Tabulation method
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    int arr[n];
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2; i<=n; i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    cout<<arr[n];
+    
+    //for printing the fibonacci series
+    for(int i=0; i<=n; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
+
+//Top down - Recursion + Memoization method
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -22,7 +47,6 @@ int main(){
 }
 
 //Without DP, With recursion
-/*
 #include <iostream>
 using namespace std;
 
@@ -45,10 +69,8 @@ int main(){
     
     return 0;
 }
-*/
 
 //Without DP, Without recursion
-/*
 #include <iostream>
 using namespace std;
 
@@ -66,4 +88,3 @@ int main(){
     
     return 0;
 }
-*/
